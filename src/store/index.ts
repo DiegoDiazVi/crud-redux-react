0998bb4a026ef191fd0725a10ae59ff3ccd61afc
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-10 12:59:50 -0500

    feat: Add RootState and AppDispatch types to store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,3 +4,6 @@ import usersReducer from "./users/slice";
 export const store = configureStore({
   reducer: { user: usersReducer },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
