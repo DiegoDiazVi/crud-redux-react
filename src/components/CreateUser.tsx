4365efcabfc7e7beafb89d76f940127808ee6306
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-12 15:54:47 -0500

    feat: Add form submission functionality to CreateUser component

--- src/components/CreateUser.tsx
+++ src/components/CreateUser.tsx
@@ -1,13 +1,38 @@
 import { Button, Card, TextInput, Title } from "@tremor/react";
+import useActionsUsers from "../hooks/useActionsUsers";
+import type { CleanUser, Email } from "../types/types";
 
 function CreateUser(): JSX.Element {
+  const { addingUser } = useActionsUsers();
+
+  const handlerSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const user: CleanUser = {
+      name: formData.get("name") as string,
+      email: formData.get("email") as Email,
+      github: formData.get("github") as string,
+    };
+
+    if (!user.name || !user.email || !user.github) {
+      alert("You must fill all the fields");
+      return;
+    }
+
+    addingUser(user);
+    form.reset();
+  };
+
   return (
     <Card style={{ marginTop: "1rem", marginBottom: "1rem" }}>
       <Title>Create new User</Title>
-      <form>
-        <TextInput placeholder="Tu nombre" />
-        <TextInput placeholder="Tu email" />
-        <TextInput placeholder="Tu GitHub User" />
+      <form onSubmit={handlerSubmit}>
+        <TextInput name="name" placeholder="Tu nombre" />
+        <TextInput name="email" placeholder="Tu email" />
+        <TextInput name="github" placeholder="Tu GitHub User" />
         <div>
           <Button type="submit" style={{ marginTop: "1rem" }}>
             Crear usuario
