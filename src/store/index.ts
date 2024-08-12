94b8bac92ff6706feb1b495b14e1acda67f1f1a8
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-12 14:51:43 -0500

    feat: Add persistandStateMiddleware to Redux store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice";
+
+const persistandStateMiddleware: Middleware = (store) => (next) => (action) => {
+  next(action);
+  localStorage.setItem("redux_state", JSON.stringify(store.getState()));
+};
 
 export const store = configureStore({
   reducer: { user: usersReducer },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(persistandStateMiddleware);
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
