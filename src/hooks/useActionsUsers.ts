4cffccc50c296b0229c0fe6c0e33f0046ef787c9
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-12 15:16:49 -0500

    feat: Add addingUser function to useActionsUsers hook

--- src/hooks/useActionsUsers.ts
+++ src/hooks/useActionsUsers.ts
@@ -1,5 +1,5 @@
-import { deleteUserByID } from "../store/users/slice";
-import { ActionsUsers, UserId } from "../types/types";
+import { addUser, deleteUserByID } from "../store/users/slice";
+import { ActionsUsers, CleanUser, UserId } from "../types/types";
 import { useAppDispatch } from "./store";
 
 function useActionsUsers(): ActionsUsers {
@@ -9,7 +9,11 @@ function useActionsUsers(): ActionsUsers {
     dispatch(deleteUserByID(id));
   };
 
-  return { deleteUser };
+  const addingUser = ({ name, email, github }: CleanUser) => {
+    dispatch(addUser({ name, email, github }));
+  };
+
+  return { deleteUser, addingUser };
 }
 
 export default useActionsUsers;
